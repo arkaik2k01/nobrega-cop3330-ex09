5a8e576ba92b030e64a45f29664a04728ccb1b11
arkaik2k01 <80938348+arkaik2k01@users.noreply.github.com>
2021-06-05 12:00:40 -0400

    Base case done. Added gitignore

--- src/main/java/BASE/App.java
+++ src/main/java/BASE/App.java
@@ -8,10 +8,12 @@ package BASE;
 //Exercise 9 - Paint Calculator
 //Sometimes you have to round up to the next number rather than follow standard rounding rules.
 //
-//Calculate gallons of paint needed to paint the ceiling of a room. Prompt for the length and width, and assume one gallon covers 350 square feet. Display the number of gallons needed to paint the ceiling as a whole number.
+//Calculate gallons of paint needed to paint the ceiling of a room. Prompt for the length and width, and assume one gallon covers 350 square feet.
+// Display the number of gallons needed to paint the ceiling as a whole number.
 //
 //Example Output
 //You will need to purchase 2 gallons of paint to cover 360 square feet.
+
 //Remember, you can’t buy a partial gallon of paint. You must
 //round up to the next whole gallon.
 //
@@ -25,10 +27,42 @@ package BASE;
 //Implement support for an L-shaped room.
 //Implement a mobile version of this app so it can be used at the hardware store.
 
+import java.util.Scanner;
+
 public class App
 {
+    static Scanner in = new Scanner(System.in);
+    final int COVERAGE = 350; //One gallon = 350 square feet
+
     public static void main(String[] args)
     {
+        App prog = new App();
+        //Input
+        System.out.print("What is the length of your room? ");
+        int len = in.nextInt();
+        System.out.print("What is the width of your room? ");
+        int wid = in.nextInt();
 
+        //Calculations
+        int squareFeet = prog.calculate(len, wid);
+        int gPerSquareFeet = prog.gallonPerSquareFeet(squareFeet);
+
+        //Output
+        prog.output(gPerSquareFeet, squareFeet);
+    }
+
+    private int calculate(int len, int wid)
+    {
+        return len * wid;
+    }
+
+    private int gallonPerSquareFeet(int squareFeet)
+    {
+        return (int) Math.ceil(((double)squareFeet)/((double)COVERAGE));
+    }
+
+    private void output(int gallon, int squareFeet)
+    {
+        System.out.println("You will need to purchase "  + gallon + " gallons of paint to cover "+ squareFeet +" square feet.");
     }
 }
